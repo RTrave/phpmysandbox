431f388317bfc6d1468cb1c601c260cc2c3cb0c1
Roman Trave <roman.trave@abadcafe.org>
2020-09-28 11:29:34 +0200

    upd: bump to release rel0.9g

--- __init.php
+++ __init.php
@@ -16,7 +16,7 @@ class MySBCore {
 
     public $version = 7;
     public $mysb_major_version = '0';
-    public $mysb_minor_version = '9f';
+    public $mysb_minor_version = '9g';
 
     public function init1() {
         global $app;
